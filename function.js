d02a947a5876f322b52b16741556eb2a7fe83cbb
rangkaidata <66771619+rangkaidata@users.noreply.github.com>
2020-08-22 22:08:39 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,22 @@
+
+/* App Step 3: logout*/
+
+'use strict';
+
+function loadXHR(url,obj,callback){
+	var request = new XMLHttpRequest();
+	var dbParam = JSON.stringify(obj);
+
+	request.onload=function(){		
+		if (request.readyState===4){
+			var paket = JSON.parse(request.responseText);
+			callback(paket);
+		}
+		else {
+			console.log('Network request failed with response ' + request.status + ': ' + request.statusText)
+		}
+	};
+	request.open('POST', url);
+	request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	request.send(dbParam);
+}
